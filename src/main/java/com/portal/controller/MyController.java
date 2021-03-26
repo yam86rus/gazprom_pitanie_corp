5cf3697d97e7300ae06076d0379eb9a300496342
Yam86rus <yam86rus@gmail.com>
2021-03-26 15:37:56 +0500

    up

--- src/main/java/com/portal/controller/MyController.java
+++ src/main/java/com/portal/controller/MyController.java
@@ -8,7 +8,9 @@ import com.portal.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -37,11 +39,39 @@ public class MyController {
     public String getInfoOnlyForManagers(Model model){
         return "view_for_managers";
     }
-
+// -------------------------- Employee----------------------------//
     @RequestMapping ("/employees")
-    public String showEmployeesPage(){
+    public String showEmployeesPage(Model model){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
         return "employees";
     }
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+
+    }
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/employees";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model){
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id){
+        employeeService.deleteEmployee(id);
+        return "redirect:/employees";
+    }
+
 
     @RequestMapping ("/news")
     public String showNewsPage(){
--- src/main/webapp/WEB-INF/view/employees.jsp
+++ src/main/webapp/WEB-INF/view/employees.jsp
@@ -6,13 +6,53 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <html>
 <head>
+    <%@ page isELIgnored="false" %>
     <title>Сотрудники</title>
 </head>
 <body>
 <%--Подключаем header--%>
 <jsp:include page="header.jsp"/>
-Сотрудники
+<h2>
+    Сотрудники
+</h2>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Department</th>
+        <th>Salary</th>
+    </tr>
+    <c:forEach var="emp" items="${allEmps}">
+        <c:url var="updateButton" value="/updateInfo">
+            <c:param name="empId" value="${emp.id}"/>
+        </c:url>
+
+        <c:url var="deleteButton" value="/deleteEmployee">
+            <c:param name="empId" value="${emp.id}"/>
+        </c:url>
+
+        <tr>
+            <td>${emp.name}</td>
+            <td>${emp.surname}</td>
+            <td>${emp.department}</td>
+            <td>${emp.salary}</td>
+            <security:authorize access="hasAnyRole('SECRETARY','ADMINISTRATOR')">
+            <td>
+                <input type="button" value="Update" onclick="window.location.href='${updateButton}'"/>
+                <input type="button" value="Delete" onclick="window.location.href='${deleteButton}'"/>
+            </td>
+            </security:authorize>
+        </tr>
+    </c:forEach>
+</table>
+
+<security:authorize access="hasAnyRole('SECRETARY','ADMINISTRATOR')">
+<input type="button" value="Добавить"
+       onclick="window.location.href = 'addNewEmployee'">
+</security:authorize>
 </body>
 </html>
